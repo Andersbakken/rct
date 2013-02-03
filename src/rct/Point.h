34a514e1503d01444e5a7c4190b4b9f1c5462f0f
Anders Bakken <agbakken@gmail.com>
2013-02-02 18:14:23 -0800

    compile

--- src/rct/Point.h
+++ src/rct/Point.h
@@ -1,6 +1,8 @@
 #ifndef Point_h
 #define Point_h
 
+#include <math.h>
+
 struct Point
 {
     Point(int xx = 0, int yy = 0) : x(xx), y(yy) {}
@@ -14,17 +16,18 @@ struct Point
     inline Point &operator*=(float c) { x = static_cast<int>(round(x *c)); y = static_cast<int>(round(y *c)); return *this; }
     inline Point &operator*=(double c) { x = static_cast<int>(round(x *c)); y = static_cast<int>(round(y *c)); return *this; }
     inline Point &operator*=(int c) { x = x *c; y = y *c; return *this; }
-    inline const Point operator+(const Point &p1, const Point &p2) { return Point(p1.x +p2.x , p1.y +p2.y ); }
-    inline const Point operator-(const Point &p1, const Point &p2) { return Point(p1.x -p2.x , p1.y -p2.y ); }
-    inline const Point operator*(const Point &p, float c) { return Point(static_cast<int>(round(p.x *c)), static_cast<int>(round(p.y *c))); }
-    inline const Point operator*(const Point &p, double c) { return Point(static_cast<int>(round(p.x *c)), static_cast<int>(round(p.y *c))); }
-    inline const Point operator*(const Point &p, int c) { return Point(p.x *c, p.y *c); }
-    inline const Point operator*(float c, const Point &p) { return Point(static_cast<int>(round(p.x *c)), static_cast<int>(round(p.y *c))); }
-    inline const Point operator*(double c, const Point &p) { return Point(static_cast<int>(round(p.x *c)), static_cast<int>(round(p.y *c))); }
-    inline const Point operator*(int c, const Point &p) { return Point(p.x *c, p.y *c); }
-    inline const Point operator-(const Point &p) { return Point(-p.x , -p.y ); }
+    inline const Point operator*(int c) const { return Point(x * c, y * c); }
+    inline const Point operator*(float c) const { return Point(x * c, y * c); }
+    inline const Point operator*(double c) const { return Point(x * c, y * c); }
+    inline const Point operator+(const Point &p) const { return Point(x + p.x, y + p.y ); }
+    inline const Point operator-(const Point &p) const { return Point(x - p.x, y - p.y ); }
+    inline const Point operator-() const { return Point(-x , -y ); }
     inline Point &operator/=(double c) {x = round(x / c); y = round(y /c); return *this;}
-    inline const Point operator/(const Point &p, double c) { return Point(static_cast<int>(round(p.x / c)), static_cast<int>(round(p.y / c))); }
+    inline const Point operator/(double c) const { return Point(static_cast<int>(round(x / c)), static_cast<int>(round(y / c))); }
+    inline Point &operator/=(float c) {x = round(x / c); y = round(y /c); return *this;}
+    inline const Point operator/(float c) const { return Point(static_cast<int>(round(x / c)), static_cast<int>(round(y / c))); }
+    inline Point &operator/=(int c) {x = round(x / c); y = round(y /c); return *this;}
+    inline const Point operator/(int c) const { return Point(static_cast<int>(round(x / c)), static_cast<int>(round(y / c))); }
 
     int x, y;
 };

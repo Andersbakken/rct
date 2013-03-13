7b785fcdb3b491be453bb024b325ad0d2ecace8d
Anders Bakken <agbakken@gmail.com>
2013-03-12 23:28:38 -0700

    Make Value support map and list.

--- src/Value.cpp
+++ src/Value.cpp
@@ -2,17 +2,39 @@
 
 void Value::clear()
 {
-    if (mType == Type_String)
+    switch (mType) {
+    case Type_String:
         stringPtr()->~String();
+        break;
+    case Type_Map:
+        mapPtr()->~Map<String, Value>();
+        break;
+    case Type_List:
+        listPtr()->~List<Value>();
+        break;
+    default:
+        break;
+    }
+
     mType = Type_Invalid;
 }
 
 void Value::copy(const Value &other)
 {
+    assert(isNull());
     mType = other.mType;
-    if (mType == Type_String) {
+    switch (mType) {
+    case Type_String:
         new (mData.stringBuf) String(*other.stringPtr());
-    } else {
+        break;
+    case Type_Map:
+        new (mData.mapBuf) Map<String, Value>(*other.mapPtr());
+        break;
+    case Type_List:
+        new (mData.listBuf) List<Value>(*other.listPtr());
+        break;
+    default:
         memcpy(&mData, &other.mData, sizeof(mData));
+        break;
     }
 }
--- src/rct/Value.h
+++ src/rct/Value.h
@@ -4,6 +4,8 @@
 #include <rct/String.h>
 #include <rct/Log.h>
 #include <rct/Serializer.h>
+#include <rct/Map.h>
+#include <rct/List.h>
 #include <math.h>
 
 class Value
@@ -14,9 +16,11 @@ public:
     inline Value(double d) : mType(Type_Double) { mData.dbl = d; }
     inline Value(bool b) : mType(Type_Boolean) { mData.boolean = b; }
     inline Value(void *ptr) : mType(Type_Pointer) { mData.pointer = ptr; }
-    Value(const String &string) : mType(Type_String) { new (mData.stringBuf) String(string); }
-    Value(const Value &other) { copy(other); }
-    Value &operator=(const Value &other) { clear(); copy(other); return *this; }
+    inline Value(const String &string) : mType(Type_String) { new (mData.stringBuf) String(string); }
+    inline Value(const Value &other) { copy(other); }
+    inline Value(const Map<String, Value> &map) : mType(Type_Map) { new (mData.mapBuf) Map<String, Value>(map); }
+    inline Value(const List<Value> &list) : mType(Type_List) { new (mData.listBuf) List<Value>(list); }
+    inline Value &operator=(const Value &other) { clear(); copy(other); return *this; }
     ~Value() { clear(); }
     inline bool isNull() const { return mType == Type_Invalid; }
     inline bool isValid() const { return mType != Type_Invalid; }
@@ -26,7 +30,9 @@ public:
         Type_Integer,
         Type_Double,
         Type_String,
-        Type_Pointer
+        Type_Pointer,
+        Type_Map,
+        Type_List
     };
     inline static const char *typeToString(Type type);
     inline Type type() const { return mType; }
@@ -35,6 +41,8 @@ public:
     inline double toDouble() const;
     inline String toString() const;
     inline void *toPointer() const;
+    inline Map<String, Value> toMap() const;
+    inline List<Value> toList() const;
     template <typename T> inline T convert(bool *ok = 0) const { invalidType(T()); if (ok) *ok = false; return T(); }
     template <typename T> static Value create(const T &t) { return Value(t); }
     void clear();
@@ -42,27 +50,36 @@ private:
     void copy(const Value &other);
     String *stringPtr() { return reinterpret_cast<String*>(mData.stringBuf); }
     const String *stringPtr() const { return reinterpret_cast<const String*>(mData.stringBuf); }
+    Map<String, Value> *mapPtr() { return reinterpret_cast<Map<String, Value>*>(mData.mapBuf); }
+    const Map<String, Value> *mapPtr() const { return reinterpret_cast<const Map<String, Value>*>(mData.mapBuf); }
+    List<Value> *listPtr() { return reinterpret_cast<List<Value>*>(mData.listBuf); }
+    const List<Value> *listPtr() const { return reinterpret_cast<const List<Value>*>(mData.listBuf); }
+
     Type mType;
     union {
         int integer;
         double dbl;
         bool boolean;
         char stringBuf[sizeof(String)];
+        char mapBuf[sizeof(Map<String, Value>)];
+        char listBuf[sizeof(List<Value>)];
         void *pointer;
     } mData;
 };
 
 const char *Value::typeToString(Type type)
 {
-  switch (type) {
-  case Type_Invalid: return "invalid";
-  case Type_Boolean: return "boolean";
-  case Type_Integer: return "integer";
-  case Type_Double: return "double";
-  case Type_String: return "string";
-  case Type_Pointer: return "pointer";
-  }
-  return "";
+    switch (type) {
+    case Type_Invalid: return "invalid";
+    case Type_Boolean: return "boolean";
+    case Type_Integer: return "integer";
+    case Type_Double: return "double";
+    case Type_String: return "string";
+    case Type_Pointer: return "pointer";
+    case Type_Map: return "list";
+    case Type_List: return "map";
+    }
+    return "";
 }
 
 
@@ -77,6 +94,8 @@ template <> inline int Value::convert<int>(bool *ok) const
     case Type_String: return atoi(stringPtr()->constData());
     case Type_Invalid: break;
     case Type_Pointer: break;
+    case Type_List: break;
+    case Type_Map: break;
     }
     if (ok)
         *ok = false;
@@ -94,6 +113,8 @@ template <> inline void* Value::convert<void*>(bool *ok) const
     case Type_String: break;
     case Type_Invalid: break;
     case Type_Pointer: return mData.pointer;
+    case Type_List: break;
+    case Type_Map: break;
     }
     if (ok)
         *ok = false;
@@ -114,6 +135,8 @@ template <> inline bool Value::convert<bool>(bool *ok) const
         const String str = toString();
         return (str == "true" || str == "1"); }
     case Type_Invalid: break;
+    case Type_List: break;
+    case Type_Map: break;
     }
     if (ok)
         *ok = false;
@@ -134,6 +157,8 @@ template <> inline double Value::convert<double>(bool *ok) const
     case Type_String: return strtod(stringPtr()->constData(), 0);
     case Type_Pointer: break;
     case Type_Invalid: break;
+    case Type_List: break;
+    case Type_Map: break;
     }
     if (ok)
         *ok = false;
@@ -153,6 +178,8 @@ template <> inline String Value::convert<String>(bool *ok) const
     case Type_String: return *stringPtr();
     case Type_Invalid: break;
     case Type_Pointer: break;
+    case Type_List: break;
+    case Type_Map: break;
     }
     if (ok)
         *ok = false;
@@ -160,16 +187,75 @@ template <> inline String Value::convert<String>(bool *ok) const
     return String();
 }
 
+template <> inline List<Value> Value::convert<List<Value> >(bool *ok) const
+{
+    if (ok)
+        *ok = true;
+
+    switch (mType) {
+    case Type_Integer: break;
+    case Type_Double: break;
+    case Type_Boolean: break;
+    case Type_String: break;
+    case Type_Invalid: break;
+    case Type_Pointer: break;
+    case Type_List: return *listPtr();
+    case Type_Map: break;
+    }
+    if (ok)
+        *ok = false;
+
+    return List<Value>();
+}
+
+template <> inline Map<String, Value> Value::convert<Map<String, Value> >(bool *ok) const
+{
+    if (ok)
+        *ok = true;
+
+    switch (mType) {
+    case Type_Integer: break;
+    case Type_Double: break;
+    case Type_Boolean: break;
+    case Type_String: break;
+    case Type_Invalid: break;
+    case Type_Pointer: break;
+    case Type_List: break;
+    case Type_Map: return *mapPtr();
+    }
+    if (ok)
+        *ok = false;
+
+    return Map<String, Value>();
+}
+
+
 inline bool Value::toBool() const { return convert<bool>(0); }
 inline int Value::toInteger() const { return convert<int>(0); }
 inline double Value::toDouble() const { return convert<double>(0); }
 inline String Value::toString() const { return convert<String>(0); }
 inline void *Value::toPointer() const { return convert<void*>(0); }
+inline Map<String, Value> Value::toMap() const { return convert<Map<String, Value> >(0); }
+inline List<Value> Value::toList() const { return convert<List<Value> >(0); }
 
 inline Log operator<<(Log log, const Value &value)
 {
+    String str;
+    {
+        Log l(&str);
+        switch (value.type()) {
+        case Value::Type_Integer: l << value.toInteger(); break;
+        case Value::Type_Double: l << value.toDouble(); break;
+        case Value::Type_Boolean: l << value.toBool(); break;
+        case Value::Type_String: l << value.toString(); break;
+        case Value::Type_Invalid: l << "(invalid)"; break;
+        case Value::Type_Pointer: l << value.toPointer(); break;
+        case Value::Type_List: l << value.toList(); break;
+        case Value::Type_Map: l << value.toMap(); break;
+        }
+    }
     log << String::format<128>("Variant(%s: %s)", Value::typeToString(value.type()),
-                               value.toString().constData());
+                               str.constData());
     return log;
 }
 
@@ -181,6 +267,8 @@ inline Serializer& operator<<(Serializer& serializer, const Value& value)
     case Value::Type_Double: serializer << value.toDouble(); break;
     case Value::Type_Boolean: serializer << value.toBool(); break;
     case Value::Type_String: serializer << value.toString(); break;
+    case Value::Type_Map: serializer << value.toMap(); break;
+    case Value::Type_List: serializer << value.toList(); break;
     case Value::Type_Pointer: error() << "Trying to serialize pointer"; break;
     case Value::Type_Invalid: break;
     }
@@ -193,10 +281,12 @@ inline Deserializer& operator>>(Deserializer& deserializer, Value& value)
     deserializer >> t;
     Value::Type type = static_cast<Value::Type>(t);
     switch (type) {
-    case Value::Type_Integer: { int v; deserializer >> v; value = Value(v); break; }
-    case Value::Type_Double: { double v; deserializer >> v; value = Value(v); break; }
-    case Value::Type_Boolean: { bool v; deserializer >> v; value = Value(v); break; }
-    case Value::Type_String: { String v; deserializer >> v; value = Value(v); break; }
+    case Value::Type_Integer: { int v; deserializer >> v; value = v; break; }
+    case Value::Type_Double: { double v; deserializer >> v; value = v; break; }
+    case Value::Type_Boolean: { bool v; deserializer >> v; value = v; break; }
+    case Value::Type_String: { String v; deserializer >> v; value = v; break; }
+    case Value::Type_Map: { Map<String, Value> v; deserializer >> v; value = v; break; }
+    case Value::Type_List: { List<Value> v; deserializer >> v; value = v; break; }
     case Value::Type_Pointer: value.clear(); error() << "Trying to deserialize pointer"; break;
     case Value::Type_Invalid: value.clear(); break;
     }

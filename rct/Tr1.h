d40858cd9aa50e86c6fc817da5bbb9bba93063cb
Jan Erik Hanssen <jhanssen@gmail.com>
2013-04-23 16:21:33 -0700

    unordered_map

--- rct/Tr1.h
+++ rct/Tr1.h
@@ -3,19 +3,23 @@
 
 #ifdef __GXX_EXPERIMENTAL_CXX0X__
 #include <memory>
+#include <unordered_map>
 #include <unordered_set>
 using std::shared_ptr;
 using std::static_pointer_cast;
 using std::weak_ptr;
 using std::enable_shared_from_this;
+using std::unordered_map;
 using std::unordered_set;
 #else
 #include <tr1/memory>
+#include <tr1/unordered_map>
 #include <tr1/unordered_set>
 using std::tr1::shared_ptr;
 using std::tr1::static_pointer_cast;
 using std::tr1::weak_ptr;
 using std::tr1::enable_shared_from_this;
+using std::tr1::unordered_map;
 using std::tr1::unordered_set;
 #endif // __GXX_EXPERIMENTAL_CXX0X__
 

25c80d70e92c839d938375a25b98a4faa5b98485
Anders Bakken <agbakken@gmail.com>
2013-08-28 19:53:51 -0700

    We never get a disconnected signal from the socket client if we close it. Not sure if we should but someone needs to tell the outside that we're gone or we will never die.

--- rct/Connection.cpp
+++ rct/Connection.cpp
@@ -182,8 +182,10 @@ void Connection::onDataWritten(const SocketClient::SharedPtr&, int bytes)
     if (!mPendingWrite) {
         if (bytes)
             mSendComplete(this);
-        if (mDone)
+        if (mDone) {
             mSocketClient->close();
+            mDisconnected(this);
+        }
     }
 }
 

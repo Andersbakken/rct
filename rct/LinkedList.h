914855453e6b99d9dde6c23096a395dbe9540d11
Anders Bakken <agbakken@gmail.com>
2013-10-26 21:04:15 -0700

    More Qt-like API

--- rct/LinkedList.h
+++ rct/LinkedList.h
@@ -15,6 +15,15 @@ public:
     int size() const { return std::list<T>::size(); }
     void append(const T &t) { std::list<T>::push_back(t); }
     void prepend(const T &t) { std::list<T>::push_front(t); }
+
+    T &first() { return std::list<T>::front(); }
+    const T &first() const { return std::list<T>::front(); }
+
+    T &last() { return std::list<T>::back(); }
+    const T &last() const { return std::list<T>::back(); }
+
+    T takeFirst() { assert(!isEmpty()); const T t = first(); std::list<T>::pop_front(); return t; }
+    T takeLast() { assert(!isEmpty()); const T t = last(); std::list<T>::pop_back(); return t; }
 };
 
 #endif

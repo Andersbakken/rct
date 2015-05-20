c052186abc78bc8a206fb0dfb0ce779a6079449c
Jan Erik Hanssen <jhanssen@netflix.com>
2015-05-20 12:02:43 -0700

    Set rpath policy for OS X

--- rct.cmake
+++ rct.cmake
@@ -1,6 +1,8 @@
 cmake_minimum_required(VERSION 2.8)
 project(rct)
 
+cmake_policy(SET CMP0042 NEW)
+
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_CURRENT_LIST_DIR}/cmake/")
 
 include(CheckSymbolExists)

2cfc60d695894deaf04bd9e6723db2f19555faf5
Anders Bakken <agbakken@gmail.com>
2015-11-02 15:24:17 -0800

    Revert "Fixes strict-aliasing rules."
    This reverts commit 11aff6ac24e4df57287de2b14d2db3cabfa5a87d.

--- rct/Value.h
+++ rct/Value.h
@@ -131,14 +131,14 @@ private:
 
     static cJSON *toCJSON(const Value &value);
     void copy(const Value &other);
-    String *stringPtr() { return reinterpret_cast<String*>(&mData.stringBuf); }
-    const String *stringPtr() const { return reinterpret_cast<const String*>(&mData.stringBuf); }
-    Map<String, Value> *mapPtr() { return reinterpret_cast<Map<String, Value>*>(&mData.mapBuf); }
-    const Map<String, Value> *mapPtr() const { return reinterpret_cast<const Map<String, Value>*>(&mData.mapBuf); }
-    List<Value> *listPtr() { return reinterpret_cast<List<Value>*>(&mData.listBuf); }
-    const List<Value> *listPtr() const { return reinterpret_cast<const List<Value>*>(&mData.listBuf); }
-    std::shared_ptr<Custom> *customPtr() { return reinterpret_cast<std::shared_ptr<Custom>*>(&mData.customBuf); }
-    const std::shared_ptr<Custom> *customPtr() const { return reinterpret_cast<const std::shared_ptr<Custom>*>(&mData.customBuf); }
+    String *stringPtr() { return reinterpret_cast<String*>(mData.stringBuf); }
+    const String *stringPtr() const { return reinterpret_cast<const String*>(mData.stringBuf); }
+    Map<String, Value> *mapPtr() { return reinterpret_cast<Map<String, Value>*>(mData.mapBuf); }
+    const Map<String, Value> *mapPtr() const { return reinterpret_cast<const Map<String, Value>*>(mData.mapBuf); }
+    List<Value> *listPtr() { return reinterpret_cast<List<Value>*>(mData.listBuf); }
+    const List<Value> *listPtr() const { return reinterpret_cast<const List<Value>*>(mData.listBuf); }
+    std::shared_ptr<Custom> *customPtr() { return reinterpret_cast<std::shared_ptr<Custom>*>(mData.customBuf); }
+    const std::shared_ptr<Custom> *customPtr() const { return reinterpret_cast<const std::shared_ptr<Custom>*>(mData.customBuf); }
 
     Type mType;
     union {

511cf7b51c8dc4bd7f3804a763f7a185323a5f27
Anders Bakken <agbakken@gmail.com>
2016-04-13 08:25:39 -0700

    Kill

--- rct/Rct.h
+++ rct/Rct.h
@@ -68,7 +68,6 @@ bool readFile(const Path& path, String &data, mode_t *perm = 0 );
 bool readFile(FILE *f, String &data, mode_t *perm = 0);
 bool writeFile(const Path& path, const String& data, int perm = -1);
 void removeDirectory(const Path &path);
-String unescape(String command);
 void findExecutablePath(const char *argv0);
 Path executablePath();
 String backtrace(int maxFrames = -1);

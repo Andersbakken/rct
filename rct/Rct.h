3ea8c7d542dc49c57589a544ca0bc20301b43c3f
Anders Bakken <agbakken@gmail.com>
2017-09-05 19:27:47 -0700

    Fix warnings with fallthrough. There's most likely a way to do this correctly with clang but I couldn't figure it out from a quick glance.

--- rct/Rct.h
+++ rct/Rct.h
@@ -268,4 +268,18 @@ String strerror(int error = errno);
         VAR = BLOCK;                            \
     } while (VAR == -1 && errno == EINTR)
 
+
+#ifndef RCT_FALL_THROUGH
+# if defined(__clang__)
+#  define RCT_FALL_THROUGH /* fall through */ struct RCT_FALL_THROUGH_STRUCT
+# elif defined(__GNUC__)
+#  if __GXX_ABI_VERSION >= 1011
+#   define RCT_FALL_THROUGH /* fall through */ __attribute__ ((fallthrough))
+#  else
+#   define RCT_FALL_THROUGH /* fall through */
+#  endif
+# else
+#  define RCT_FALL_THROUGH /* fall through */ struct RCT_FALL_THROUGH_STRUCT
+# endif
+#endif
 #endif

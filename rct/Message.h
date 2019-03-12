2a3fb3ec22d0553f66bffdf517072ac49b149cbf
Anders Bakken <agbakken@gmail.com>
2019-03-11 20:25:13 -0700

    Add a clear cache function.

--- rct/Message.h
+++ rct/Message.h
@@ -21,6 +21,13 @@ public:
     virtual ~Message()
     {}
 
+    void clearCache()
+    {
+        mHeader.clear();
+        mValue.clear();
+        mVersion = 0;
+    }
+
     enum Flag {
         None = 0x0,
         Compressed = 0x1,
@@ -33,7 +40,7 @@ public:
     virtual void encode(Serializer &/* serializer */) const = 0;
     virtual void decode(Deserializer &/* deserializer */) = 0;
 
-    virtual size_t encodedSize() const { return -1; }
+    virtual size_t encodedSize() const { return String::npos; }
     enum MessageErrorType {
         Message_Success,
         Message_VersionError,
